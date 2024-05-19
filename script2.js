62de5e579aa97ec9d7291db85ebc1043af551341
ValentiLo <125864669+ValentiLo@users.noreply.github.com>
2024-05-19 22:23:56 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,4 @@
+var specialProduct = document.querySelector('.product:nth-child(2)');
+console.log(specialProduct);
+var unavailableProduct = document.querySelector('li:last-child');
+console.log(unavailableProduct);
