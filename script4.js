d5828ba5b0e065612b8ec107e2b3133f77be0117
ValentiLo <125864669+ValentiLo@users.noreply.github.com>
2024-05-20 13:36:51 +0300

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,23 @@
+var catalogData = [
+    {
+      isAvailable: true,
+      isSpecial: false
+    },
+    {
+      isAvailable: false,
+      isSpecial: false
+    },
+    {
+      isAvailable: true,
+      isSpecial: true
+    },
+    {
+      isAvailable: true,
+      isSpecial: false
+    },
+    {
+      isAvailable: false,
+      isSpecial: false
+    }
+  ];
+  
